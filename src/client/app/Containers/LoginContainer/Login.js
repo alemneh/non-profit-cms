52efbd924090f4331f5d5049ea91a910c64b1859
Alemneh <Alemneh@hotmail.com>
2017-02-08 00:35:22 -0800

    add login container

---
+++ src/client/app/Containers/LoginContainer/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { login } from '../actions/loginActions';
+import Login from '../components/LoginComponent/Login';
+
+
+class LoginContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      error: null
+    }
+
+    this.handleLogin          = this.handleLogin.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleUsernameChange(e) {
+    this.setState( {username: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+    this.setState( { password: e.target.value});
+  }
+
+  handleLogin(e) {
+      e.preventDefault();
+      const { username, password } = this.state;
+      const validateLoginInput = this.validateLoginInput(username, password);
+      if(validateLoginInput) {
+        this.setState({
+          error: validateLoginInput
+        });
+        return;
+      }
+
+      this.props.login(username, password);
+  }
+
+  validateLoginInput(username, password) {
+    if(!username) {
+      return 'Please enter username';
+    } else if(!password) {
+      return 'Please enter password';
+    } else {
+      return null;
+    }
+  }
+
+
+  render() {
+    const { handleLogin, handleUsernameChange, handlePasswordChange} = this;
+    return (
+      <section>
+        <Login handleLogin={ handleLogin }
+               handleUsernameChange={ handleUsernameChange }
+               handlePasswordChange={ handlePasswordChange }/>
+      </section>
+    )
+  }
+}
+
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ login }, dispatch);
+}
+
+export default connect(matchDispatchToProps)(Login)
