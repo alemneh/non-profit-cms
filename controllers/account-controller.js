1747f8514e3ebb9c872191b3e08a3944a8b3b50f
Alemneh <Alemneh@hotmail.com>
2017-02-28 23:38:50 -0800

    added correct url for deployment

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -6,7 +6,7 @@ const AccountRoutes= require('../routes/account-routes.js');
 module.exports = (accountRouter, models) => {
 
   accountRouter.route('/accounts')
-    .post(jwtAuth, (req, res) => {AccountRoutes.createAccount(req, res); })
+    .post( (req, res) => {AccountRoutes.createAccount(req, res); })
     .get(jwtAuth, (req, res) => {AccountRoutes.getAllAccounts(req, res); });
 
 };
